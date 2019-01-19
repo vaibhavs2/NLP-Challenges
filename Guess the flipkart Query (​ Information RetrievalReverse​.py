a0ea0527063511079c47f5baf80e97ada0384f23
vikash vaibhav <vaibhavk106@gmail.com>
2019-01-20 04:11:47 +0530

    Update Guess the flipkart Query (​ Information RetrievalReverse​.py

--- Guess the flipkart Query (​ Information RetrievalReverse​.py
+++ Guess the flipkart Query (​ Information RetrievalReverse​.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from fuzzywuzzy import process
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
-column=['product','catagory']
-try:
-    data = pd.read_csv('/home/next/Desktop/training.txt', sep=r'\t',header = None,names=column)
-    catagory=[r for r in data['catagory'].unique()]
-except FileNotFoundError:
-        print("training file doesn't exists")
-product=input()
+column = ['product','category']
+data = pd.read_csv('/home/next/Desktop/training.txt',sep='\t',names=column) #addressing to training file in system
+vector = CountVectorizer()
+vector.fit(data['product'][1:])
+counts = vector.transform(data['product'][1:])
+mn= MultinomialNB()
+mn.fit(counts,data['category'][1:])
+print('Enter number of queries line')
+number_of_lines=int(input())
+lines=[]
+print('paste queries')
+for i in range(number_of_lines):    
+    query=input()
+    lines.append(query)
 
-for i in range(0,len(product),60):
-    chunk=product[i:i+60]
-    print(process.extractOne(chunk, catagory)[0])
+line=mn.predict(vector.transform(lines))
+
+for ansr in line:
+        print(ansr)
+    
