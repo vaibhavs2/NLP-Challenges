0611ea68807452a08275b2dc89a0d396919c833a
vikash vaibhav <vaibhavk106@gmail.com>
2019-01-20 15:55:04 +0530

    Update Guess the flipkart Query (​ Information RetrievalReverse​.py

--- Guess the flipkart Query (​ Information RetrievalReverse​.py
+++ Guess the flipkart Query (​ Information RetrievalReverse​.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
 import pandas as pd
 column = ['product','category']
-data = pd.read_csv('/home/next/Desktop/training.txt',sep='\t',names=column) #addressing to training file in system
+data = pd.read_csv('/home/next/Desktop/training.txt',sep='\t',names=column)
 vector = CountVectorizer()
 vector.fit(data['product'][1:])
 counts = vector.transform(data['product'][1:])
@@ -13,14 +11,22 @@ mn= MultinomialNB()
 mn.fit(counts,data['category'][1:])
 print('Enter number of queries line')
 number_of_lines=int(input())
-lines=[]
+result=[]
 print('paste queries')
 for i in range(number_of_lines):    
     query=input()
-    lines.append(query)
+    l=[]
+    extract_nouns = word_tokenize(query)
+    extract_nouns = nltk.pos_tag(extract_nouns)
+    for each in extract_nouns:
+        if(each[1] in ('NN','NNS','NNP','NNPS','SYM')):    
+            l.append(each[0])
+        else:
+                continue
+    query=' '.join(l)
+    result.append(query)
 
-line=mn.predict(vector.transform(lines))
 
-for ansr in line:
+for ansr in mn.predict(vector.transform(result)):
         print(ansr)
     
